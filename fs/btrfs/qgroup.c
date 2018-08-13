326743c7698649124efe42a429c70d5c67c4cc97
Jeff Mahoney <jeffm@suse.com>
2018-08-13 16:10:46 -0400

    btrfs: free ulists in callers of btrfs_qgroup_account_extent
    btrfs_qgroup_account_extent currently frees the ulist arguments
    if they're non-NULL.  This makes it a pain to do simpler error
    handling in btrfs_qgroup_account_extents since we end up with
    a partially-released extent record that we need to handle
    specially.
    
    Signed-off-by: Jeff Mahoney <jeffm@suse.com>

--- fs/btrfs/qgroup.c
+++ fs/btrfs/qgroup.c
@@ -2125,8 +2125,6 @@ out:
 out_free:
 	ulist_free(tmp);
 	ulist_free(qgroups);
-	ulist_free(old_roots);
-	ulist_free(new_roots);
 	return ret;
 }
 
@@ -2195,8 +2193,6 @@ int btrfs_qgroup_account_extents(struct btrfs_trans_handle *trans)
 							  record->num_bytes,
 							  record->old_roots,
 							  new_roots);
-			record->old_roots = NULL;
-			new_roots = NULL;
 		}
 cleanup:
 		ulist_free(record->old_roots);
@@ -2732,6 +2728,8 @@ static int qgroup_rescan_leaf(struct btrfs_trans_handle *trans,
 		/* For rescan, just pass old_roots as NULL */
 		ret = btrfs_qgroup_account_extent(trans, found.objectid,
 						  num_bytes, NULL, roots);
+		ulist_free(roots);
+		roots = NULL;
 		if (ret < 0)
 			goto out;
 	}
